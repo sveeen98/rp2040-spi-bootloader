e6adfc4ad062c2e3ce404110ea4ff0f52b346897
Sven Koch <sven@svenkoch.eu>
2025-01-18 12:43:10 +0100

    Make reboot library compatible with C++

--- reboot/include/bootloader/reboot.h
+++ reboot/include/bootloader/reboot.h
@@ -1,20 +1,33 @@
 /**
- * Copyright (c) 2022 Brian Starkey <stark3y@gmail.com>
+ * @file reboot.h
+ * @brief Reboot the RP2040 into the bootloader.
+ * 
+ * This project is a fork of Brian Starkey's RP2040 Serial Bootloader. The original project can be found at 
+ * https://github.com/usedbytes/rp2040-serial-bootloader.git
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * @author Sven Koch
+ * @date 2025
+ * @license BSD-3-Clause
  */
-
 #ifndef _BOOTLOADER_REBOOT_H_
 #define _BOOTLOADER_REBOOT_H_
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include <stdbool.h>
 
-// The bootloader can be entered in three ways:
-//  - BOOTLOADER_ENTRY_PIN is low
-//  - Watchdog scratch[5] == BOOTLOADER_ENTRY_MAGIC && scratch[6] == ~BOOTLOADER_ENTRY_MAGIC
-//  - No valid image header
+// Magic value for the watchdog scratch register to indicate 
+// that the bootloader should be entered on the next reset.
 #define BOOTLOADER_ENTRY_MAGIC 0xb105f00d
 
-void bootloader_reboot(bool to_bootloader);
+void bootloader_reboot(bool);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _BOOTLOADER_REBOOT_H_ */
